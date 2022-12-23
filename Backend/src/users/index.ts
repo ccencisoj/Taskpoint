210ff797aa187f101b1640480ecd45e799381a6c
ascife <bizapro123@gmail.com>
2022-12-22 21:08:15 -0500

    Revert "se agrego las vistas de inicio de sesion"
    This reverts commit 23ae572edbe932c2b5716700d0dd25c00a6bc630.

--- Backend/src/users/index.ts
+++ Backend/src/users/index.ts
@@ -1,8 +1,7 @@
 import { Router } from "express";
 import { GetUsersController } from "./useCases/GetUsers/GetUsersController";
-import { LoginUserController } from "./useCases/LoginUser/LoginUserController";
 import { DeleteUserController } from "./useCases/DeleteUser/DeleteUserController";
-import { LogoutUserController } from "./useCases/LogoutUser/LogoutUserController";
+import { LoginUserController } from "./useCases/LoginUser/LoginUserController";
 import { RegisterUserController } from "./useCases/RegisterUser/RegisterUserController";
 import { GetCurrentUserController } from './useCases/GetCurrentUser/GetCurrentUserController';
 
@@ -10,7 +9,6 @@ const router = Router();
 
 router.get("/users", GetUsersController.execute);
 router.post("/users/login", LoginUserController.execute);
-router.post("/users/logout", LogoutUserController.execute);
 router.post("/users/register", RegisterUserController.execute);
 router.get("/users/current", GetCurrentUserController.execute);
 router.delete("/users/:id", DeleteUserController.execute);
